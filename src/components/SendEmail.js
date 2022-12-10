6ad1fa4d218bfbf504e4b281673d0d061b6c25e7
unknown <nurul.islam96@aol.com>
2022-12-10 22:35:18 +0600

    fixed email bugs

--- src/components/SendEmail.js
+++ src/components/SendEmail.js
@@ -1,4 +1,5 @@
 import emailjs from "emailjs-com";
+import { toast } from "react-hot-toast";
 
 const serviceId = "service_0gx2apt";
 const templateId = "template_oe2h7un";
@@ -12,9 +13,10 @@ const SendEmail = async(name,email,message) => {
             {name,email,message},
             userId
         )
-        if(response.status === 200){console.log("Success")}
+        if(response.status === 200){toast.success("Sent Mail Successfully")}
     } catch (error) {
         console.log("Failed to send Email", error)
+        toast.error("Failed to sent Mail. Try again!")
     }
 };
 
